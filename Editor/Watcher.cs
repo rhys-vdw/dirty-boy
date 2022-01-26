db11ef9ee140d76d179a0e3074085d8ecd9dde29
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-01-26 20:50:14 +1100

    wip

--- Editor/Watcher.cs
+++ Editor/Watcher.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEditor;
+using System.Text;
+using System.Collections.Generic;
 
 namespace DirtyBoy {
   class Watcher : AssetPostprocessor {
@@ -9,23 +11,37 @@ namespace DirtyBoy {
       string[] movedAssets,
       string[] movedFromAssetPaths
     ) {
+      var reserializePaths = new List<string>();
       foreach (string path in importedAssets) {
         var script = (MonoScript) AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
         if (script == null) continue;
         var scriptClass = script.GetClass();
         if (scriptClass == null) continue;
-        if (typeof(ScriptableObject).IsAssignableFrom(scriptClass)) {
-          var guids = AssetDatabase.FindAssets($"t:{scriptClass}");
-          foreach (var guid in guids) {
-            var assetPath = AssetDatabase.GUIDToAssetPath(guid);
-            var o = AssetDatabase.LoadAssetAtPath(assetPath, scriptClass);
-            if (o == null) {
-              Debug.LogWarning($"Expected to find {scriptClass} at '{assetPath}'");
-            } else {
-              EditorUtility.SetDirty(o);
+        if (typeof(UnityEngine.Object).IsAssignableFrom(scriptClass)) {
+          var objects = Resources.FindObjectsOfTypeAll(scriptClass);
+          if (objects.Length > 0) {
+            foreach (var obj in objects) {
+              var component = obj as Component;
+              if (component != null && PrefabUtility.IsPartOfPrefabAsset(component)) {
+                Debug.Log("persistent? " + EditorUtility.IsPersistent(component));
+                var prefab = component.transform.root.gameObject;
+                var prefabPath = AssetDatabase.GetAssetPath(prefab);
+                EditorUtility.SetDirty(prefab);
+                reserializePaths.Add(prefabPath);
+              } else if (EditorUtility.IsPersistent(obj)) {
+                var assetPath = AssetDatabase.GetAssetPath(obj);
+                if (!string.IsNullOrEmpty(assetPath)) {
+                  reserializePaths.Add(assetPath);
+                }
+              }
             }
           }
         }
+      }
+
+      if (reserializePaths.Count > 0) {
+        AssetDatabase.ForceReserializeAssets(reserializePaths);
+        Debug.Log($"Reserialized:\n - {string.Join("\n - ", reserializePaths)}");
       }
     }
   }

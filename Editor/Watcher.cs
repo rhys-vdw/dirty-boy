7ad10c6c134e3cba52e0cbffae120b1619168bfe
Rhys van der Waerden <rhys.vdw@gmail.com>
2022-01-26 19:54:00 +1100

    Add ScriptableObject watcher

---
+++ Editor/Watcher.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace DirtyBoy {
+  class Watcher : AssetPostprocessor {
+    static void OnPostprocessAllAssets(
+      string[] importedAssets,
+      string[] deletedAssets,
+      string[] movedAssets,
+      string[] movedFromAssetPaths
+    ) {
+      foreach (string path in importedAssets) {
+        var script = (MonoScript) AssetDatabase.LoadAssetAtPath(path, typeof(MonoScript));
+        if (script == null) continue;
+        var scriptClass = script.GetClass();
+        if (scriptClass == null) continue;
+        if (typeof(ScriptableObject).IsAssignableFrom(scriptClass)) {
+          var guids = AssetDatabase.FindAssets($"t:{scriptClass}");
+          foreach (var guid in guids) {
+            var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            var o = AssetDatabase.LoadAssetAtPath(assetPath, scriptClass);
+            if (o == null) {
+              Debug.LogWarning($"Expected to find {scriptClass} at '{assetPath}'");
+            } else {
+              EditorUtility.SetDirty(o);
+            }
+          }
+        }
+      }
+    }
+  }
+}
